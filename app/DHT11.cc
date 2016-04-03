6652c8ae7c2db700f391de34f746ce37580aa825
Yanhui Shen <shen.elf@gmail.com>
2016-04-03 18:54:33 +0800

    ~ fix busy read

--- app/DHT11.cc
+++ app/DHT11.cc
@@ -1,4 +1,4 @@
-// c++ -O2 -std=c++14 DHT11.cc -lgpio -I../lib
+// c++ -O2 -std=c++14 -lgpio -I../lib DHT11.cc 
 
 #include <iostream>
 #include <DHT11.h>
@@ -21,8 +21,8 @@ int main(int argc, char* argv[]) {
         if (ok == DHT11::RESULT::OK) {
             std::cout << humidity << ", " << temperature << std::endl;
             using namespace std::chrono_literals;
-            std::this_thread::sleep_for(500ms);
         };
+        std::this_thread::sleep_for(500ms);
     }
 
     exit(0);
